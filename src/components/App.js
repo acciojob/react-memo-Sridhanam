5185d392f0eab6ee51f5ff7a86c55d2669498f08
Sridhanam <dharsri255@gmail.com>
2025-04-07 18:34:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,32 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
+import UseMemo from "./UseMemo";
+import ReactMemo from "./ReactMemo";
 
-const UseMemo = () => {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [tasks, setTasks] = useState([]);
 
-  const result = useMemo(() => {
-    return count * 2;
-  }, [count]);
-
-  const onIncrease = () => {
-    setCount(count + 1);
+  const onAdd = () => {
+    setTasks((prev) => [...prev, "New Todo"]);
   };
   return (
-    <div>
-      <div>
-        <span>Count:{count}</span>
-        <button onClick={onIncrease}>+</button>
-      </div>
-      <h2>Expensive Calculation</h2>
-      {result}
+    <div id="main">
+      <h1>React.useMemo</h1>
+      <h2>My todos</h2>
+      <ul>
+        {tasks.map((i, index) => (
+          <li key={index}>{i}</li>
+        ))}
+      </ul>
+
+      <button onClick={onAdd}>Add Todo</button>
+
+      <hr />
+      <UseMemo />
+      <hr />
+      <hr />
+      <ReactMemo />
     </div>
   );
 };
 
-export default UseMemo;
+export default App;
